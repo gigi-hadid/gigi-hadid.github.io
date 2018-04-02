39cb7edb23c6eedeccaf4a1689560d079fa232ad
Мясное котясо <nadya.kott@gmail.com>
2018-04-03 04:45:39 +0500

    lp
    landing page for gipsolit

---
+++ brsync_temp.bat
@@ -0,0 +1,2 @@
+cd C:\Users\cat\WebstormProjects\gipsolit-landingPage
+browser-sync start -s -f "*.html, css/*.css"
---
+++ js/slickslider-features.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('.features-slider').slick({
+});
+});
